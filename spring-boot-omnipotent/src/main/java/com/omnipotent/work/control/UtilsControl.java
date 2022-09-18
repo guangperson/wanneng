537c9c5d0376e4384a4bd6f3b9913c5bdad4e002
unknown <guangperson>
2022-09-18 23:34:59 +0800

    验证问题

--- spring-boot-omnipotent/src/main/java/com/omnipotent/work/control/UtilsControl.java
+++ spring-boot-omnipotent/src/main/java/com/omnipotent/work/control/UtilsControl.java
@@ -4,6 +4,8 @@ import com.omnipotent.work.service.impl.RedisServiceImpl;
 import com.omnipotent.work.utils.CodeUtils;
 import com.omnipotent.work.utils.VerificationCodeImage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +25,12 @@ public class UtilsControl {
     RedisServiceImpl redisService;
 
     @RequestMapping("/imgCode")
-    public void imgcode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void imgcode(@CookieValue(name = "codeCookie",required = false)String cookie, HttpServletRequest request, HttpServletResponse response) throws IOException, InterruptedException {
         //生成验证码
         String code = CodeUtils.getCode4Hard();
+        String remortIP = getRemortIP(request);
+        System.out.println("用户ip访问："+remortIP);
+        System.out.println("用户cookie："+cookie);
         //将验证码存储redis中
         redisService.setValue("code",code);
         System.out.println("当前验证码"+code);
@@ -38,5 +43,21 @@ public class UtilsControl {
 
         //将图片通过respose输出流输出图片
         ImageIO.write(new VerificationCodeImage(code).createImg(), "JPEG", response.getOutputStream());
+    }
+
+
+    /**
+     * 获得访问者真实ip
+     * @param request
+     * @return
+     */
+    public String getRemortIP(HttpServletRequest request) {
+        String ip = "";
+        if (request.getHeader("x-forwarded-for") == null) {
+            ip = request.getRemoteAddr();
+        } else {
+            ip = request.getHeader("x-forwarded-for");
+        }
+        return ip.split(",")[0];
     }
 }
